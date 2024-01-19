acc2b226dab7a10c0541797949fef74506db0012
Safarqwerty <safar.alam79@gmail.com>
2024-01-19 13:21:06 +0800

    Seed Admin User

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CampTableSeeder::class,
             CampBenefitTableSeeder::class,
+            AdminUserSeeder::class,
         ]);
     }
 }
