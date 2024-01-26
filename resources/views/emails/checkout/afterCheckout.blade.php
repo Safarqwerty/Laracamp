bc895a5f20c56cefe5132062d15c9c842ce8d253
Safarqwerty <safar.alam79@gmail.com>
2024-01-26 23:32:12 +0800

    perbaikan route dashboard

--- resources/views/emails/checkout/afterCheckout.blade.php
+++ resources/views/emails/checkout/afterCheckout.blade.php
@@ -5,7 +5,7 @@ Hi {{ $checkout->User ? $checkout->User->name : 'User' }}
 <br>
 Anda telah mendaftar di kelas <b>{{ $checkout->Camp->title }}</b>, jangan lupa bayar yahh!
 
-<x-mail::button :url="route('dashboad')">
+<x-mail::button :url="route('user.dashboard')">
 My Dashboard
 </x-mail::button>
 
