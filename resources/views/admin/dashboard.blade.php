c136037d4efea8fe53c19f293583afb6be6d69ae
Safarqwerty <safar.alam79@gmail.com>
2024-01-27 01:13:01 +0800

    mengatur ulang tampilan dashboard admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,61 +1,67 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="dashboard my-5">
     <div class="container">
-        <div class="row text-left">
-            <div class=" col-lg-12 col-12 header-wrap mt-4">
-                <p class="story">
-                    DASHBOARD
-                </p>
-                <h2 class="primary-header ">
-                    My Bootcamps
-                </h2>
+        <div class="row">
+            <div class="col-8 offset-2">
+                <div class="card">
+                    <div class="card-header">
+                        My Camps
+                    </div>
+                    <div class="card-body">
+                        @include('components.alert')
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>User</th>
+                                    <th>Camp</th>
+                                    <th>Harga</th>
+                                    <th>Register</th>
+                                    <th>Pembayaran</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($checkouts as $checkout)
+                                    <tr>
+                                        <td>
+                                            {{ $checkout->User->name }}
+                                        </td>
+                                        <td>
+                                            {{ $checkout->Camp->title }}
+                                        </td>
+                                        <td>
+                                            {{ $checkout->Camp->price }}k
+                                        </td>
+                                        <td>
+                                            {{ $checkout->created_at->format('M d Y') }}
+                                        </td>
+                                        <td>
+                                            @if ($checkout->is_paid)
+                                                <span class="badge bg-success">Sudah Bayar</span>
+                                            @else
+                                                <span class="badge bg-danger">Belum Bayar</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <form action="" method="post">
+                                                @csrf
+                                                <button class="btn btn-primary btn-sm">Set Bayar</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="3">
+                                            No camps registered
+                                        </td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row my-5">
-            @include('components.alert')
-            <table class="table">
-                <tbody>
-                    @forelse ($checkouts as $checkout)
-                    <tr class="align-middle">
-                        <td width="18%">
-                            <img src="{{ asset('images/item_bootcamp.png') }}" height="120" alt="">
-                        </td>
-                        <td>
-                            <p class="mb-2">
-                                <strong>{{ $checkout->Camp->title }}</strong>
-                            </p>
-                            <p>
-                                {{ $checkout->created_at->format('M, d, Y') }}
-                            </p>
-                        </td>
-                        <td>
-                            <strong>${{ $checkout->Camp->price }}k</strong>
-                        </td>
-                        <td>
-                            @if ($checkout->is_paid)
-                                <strong class="text-success">Payment Success</strong>
-                            @else
-                                <strong class="text-danger">Waiting for Payment</strong>
-                            @endif
-                        </td>
-                        <td>
-                            <a href="https://wa.me/0895423468666?text=woi saya mau beli kelas {{ $checkout->Camp->title }}" class="btn btn-primary">
-                                Contact Support
-                            </a>
-                        </td>
-                    </tr>
-                    @empty
-                        <tr>
-                            <td colspan="5">
-                                <h3>Data Kosong</h3>
-                            </td>
-                        </tr>
-                    @endforelse
-                </tbody>
-            </table>
-        </div>
     </div>
-</section>
 @endsection
